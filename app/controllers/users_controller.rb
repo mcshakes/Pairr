63b0d6ef7309e2b0ff03c639d0276e3bc65e86f2
Edward McCarthy <efmccarthy26@gmail.com>
2015-06-23 15:17:56 -0600

    had to skip the before_action within the static pages so the user can actually login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,32 @@
 class UsersController < ApplicationController
+
+  def show
+    if additional_info_added?
+      # if the person has the info needed to move on, send to dashboard/matches
+      # matches = Match.all will be an index of all possible matches
+    else
+      redirect_to edit_user_path
+    end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+    # redirect_to user_path()
+  end
+
+  private
+
+  def additional_info_added?
+    # current_user.preferred_languages and don't forget below
+    current_user.details
+  end
+
+  def user_params
+    params.require(:user).permit(:details)
+  end
 end
