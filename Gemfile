b7182db903e4608136b3ba61be3b4217f026c66c
Edward McCarthy <efmccarthy26@gmail.com>
2015-06-23 14:49:37 -0600

    added a user controller to have a user_edit action, where person will add list of languages and such. This needs to happen before th move on to the dashboard page

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'omniauth-github'
 
 group :development, :test do
   gem 'byebug'
+  gem 'pry'
   gem 'web-console', '~> 2.0'
   gem 'better_errors'
   gem 'binding_of_caller'
---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+  def self.find_or_create_from_auth(data)
+    user = User.find_or_create_by(provider: data.provider, uid: data.uid)
+    # binding.pry into here to see data.info
+    user.nickname = data.info.nickname
+    user.image_url = data.info.image_url
+
+    user.save
+  end
+end
