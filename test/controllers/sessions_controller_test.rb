6172909a0bd8b143c33bee023f4fe61edf7a5e45
Edward McCarthy <efmccarthy26@gmail.com>
2015-06-23 18:51:26 -0600

    failed to stub User with oauth web_mocks. Every guide on the web uses rspec. Moving on. Ain't nobody got time for that

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -2,4 +2,18 @@ require "test_helper"
 
 class SessionsControllerTest < ActionController::TestCase
 
+  # def setup
+  #   User.create = { 'provider' => 'github',
+  #                     'info' => {
+  #                       "image_url" => "fake_mocker_thumbnail",
+  #                       'nickname' => 'NatashaTheRobot'
+  #                     }
+  #   }
+  # end
+  #
+  # OmniAuth.config.add_mock(:github, user)
+  #
+  # test "users size has increased dramatically" do
+  #   assert_equal 1, User.count
+  # end
 end
